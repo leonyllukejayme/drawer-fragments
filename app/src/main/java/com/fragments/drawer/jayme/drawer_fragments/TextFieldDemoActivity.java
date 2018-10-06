d54cbb8a17ffa07cd32d9afd6e87193ba6328f4d
leonyllukejayme <leonyljayme@gmail.com>
2018-10-06 17:54:04 +0800

    My commit message

---
+++ app/src/main/java/com/fragments/drawer/jayme/drawer_fragments/TextFieldDemoActivity.java
@@ -0,0 +1,23 @@
+package com.fragments.drawer.jayme.drawer_fragments;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TextFieldDemoActivity extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.activity_text_field_demo, container, false);
+
+
+
+        return layout;
+    }
+}
+
